9afc05b5149ea62b4151c8a5579429b96ccfc7e8
Daniil <Dan4ikbite@gmail.com>
2019-10-21 18:37:28 +0300

    Initial commit

--- GiphyTestTask/AppDelegate.swift
+++ GiphyTestTask/AppDelegate.swift
@@ -10,12 +10,20 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        if let window = window {
+            let viewController = ViewController()
+            window.rootViewController = viewController
+            window.makeKeyAndVisible()
+        }
+            
         return true
+        
     }
 
     // MARK: UISceneSession Lifecycle
